4bd6922bc2c19de366f4b3049662c0e900931ed6
ravindra.kumar <ravindra.kumar@servicenow.com>
2018-12-21 23:05:20 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ravindra Kumar on 12/15/18.
+//  Copyright © 2018 Ravindra Kumar. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
